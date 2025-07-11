ab3f563297fca12ab716ee72a128a38297e8c1de
Evgeny Metelkin <evgeny.metelkin@gmail.com>
2025-07-11 13:29:37 +0300

    add syntax highlighting support

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,15 @@
 const sitemap = require("@quasibit/eleventy-plugin-sitemap");
 
-module.exports = function(eleventyConfig) {
+// Prism highlighter
+// npm install --save-dev @11ty/eleventy-plugin-syntaxhighlight
+const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+
+// TODO: switch to shiki highlighter, supports heta
+// eleventy-plugin-shiki-twoslash not working
+// https://samwarnick.com/blog/adding-shiki-to-eleventy-3/
+// https://stefanzweifel.dev/posts/2024/06/03/how-i-use-shiki-in-eleventy/
+
+module.exports = async function(eleventyConfig) {
   // create sitemap
   eleventyConfig.addPlugin(sitemap, {
     sitemap: {
@@ -12,6 +21,9 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("content/robots.txt");
   eleventyConfig.addPassthroughCopy("content/assets");
 
+  // add syntax highlighting plugin
+  eleventyConfig.addPlugin(syntaxHighlight);
+  
   return {
     dir: {
       input: ".",        // default
